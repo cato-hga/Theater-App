5145e9589cc6038f7676148f6ac3f856cb571cba
John C Cato <hga813@gmail.com>
2015-03-06 13:36:45 -0500

    Add region to endpoint

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["S3_ID"],                        # required
     aws_secret_access_key: ENV["S3_SecretKey"],                        # required
-    endpoint:'http://s3.amazonaws.com',
+    endpoint:'http://s3-us-west-2.amazonaws.com',
     region: 'us-west-2'
   }
   config.fog_directory  = ENV["S3_Bucket"]                         # required
